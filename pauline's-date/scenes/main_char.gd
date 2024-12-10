37087c14eba57a0184d2a03bb61d9934e37f437b
Jennifer Lai <jenniferlai43@gmail.com>
2024-12-10 02:38:23 -0500

    [#1] Add continued player movement with input rejection and taller NPC static body

--- pauline's-date/scenes/main_char.gd
+++ pauline's-date/scenes/main_char.gd
@@ -10,21 +10,14 @@ const JUMP_VELOCITY = -625.0
 @onready var jump_sfx: AudioStreamPlayer = $jump_sfx
 
 func _physics_process(delta: float) -> void:
-	
-	if (game_manager.is_cutscene || game_manager.dialogue_is_active):
-		# Make sure pauline is idle during dialogue scenes.
-		sprite_2d.animation = "pauline_idle"
-	
-	# If this is a cutscene, user is not allowed to click anything.
-	if (game_manager.is_cutscene == true):
-		return
+	var can_accept_input = not (game_manager.is_cutscene || game_manager.dialogue_is_active)
 	
 	# If character is in dialogue, do allow clicking "next" on the text until
 	# there is no more text to display.
 	if (game_manager.dialogue_is_active == true):
 		if Input.is_action_just_pressed("interact"):
 			dialogue.display_next_text()
-		return
+		#return
 	
 	if (velocity.x > 1 || velocity.x < -1):
 		sprite_2d.animation = "pauline_running"	
@@ -37,17 +30,18 @@ func _physics_process(delta: float) -> void:
 		sprite_2d.animation = "pauline_jump"
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if can_accept_input and Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		jump_sfx.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, 10)
+	if can_accept_input:
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, 10)
 
 	move_and_slide()
 	
